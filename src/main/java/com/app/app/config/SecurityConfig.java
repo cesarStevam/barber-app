d1f32162c228b0d3c3cd067217a6bef49d57f21e
stevenrz31 <stevenrz2006@gmail.com>
2024-11-20 18:20:16 -0500

    Agregue el handler para que cada cierto rol se una, muestra la vista correspondiente, la vista usuario y barbero no tiene, hay que igual crear un html unico para los 3

--- src/main/java/com/app/app/config/SecurityConfig.java
+++ src/main/java/com/app/app/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,59 +19,51 @@ import lombok.RequiredArgsConstructor;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                return http
-                                .csrf(AbstractHttpConfigurer::disable)
-                                .authorizeHttpRequests(auth -> auth
-                                                .requestMatchers("/", "/index.html", "/barberia6.jpg", "/Logo.jpeg",
-                                                                "/static", "/resources", "/Fondo.jpeg", "/BuzzCut.jpeg",
-                                                                "/Ceradecabello.jpg", "/cortebarberia.jpeg",
-                                                                "/Cremadeafeitar.jpg",
-                                                                "/Cremaparaelcabello.jpg", "/Gelparaelcabello.jpg",
-                                                                "/TaperFade.jpg",
-                                                                "/Lowfadee.jpg", "/Mohicano.jpg", "/templates",
-                                                                "/register",
-                                                                "/style.css", "/public/**")
-                                                .permitAll()
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/index.html", "/barberia6.jpg", "/Logo.jpeg", "/static", "/resources",
+                                "/Fondo.jpeg", "/BuzzCut.jpeg", "/Ceradecabello.jpg", "/cortebarberia.jpeg",
+                                "/Cremadeafeitar.jpg", "/Cremaparaelcabello.jpg", "/Gelparaelcabello.jpg",
+                                "/TaperFade.jpg", "/Lowfadee.jpg", "/Mohicano.jpg", "/templates", "/register",
+                                "/style.css", "/public/**")
+                        .permitAll()
 
-                                                // Rutas para administrador
-                                                .requestMatchers("/admin/**", "/personas").hasRole("administrador")
+                        // Rutas para administrador
+                        .requestMatchers("/admin/**", "/agregarreserva", "/personas", "/reservas").hasRole("administrador")
 
-                                                // Rutas para usuario
-                                                .requestMatchers("/user/**").hasRole("usuario")
+                        // Rutas para usuario
+                        .requestMatchers("/user/**").hasRole("usuario")
 
-                                                // Rutas para barbero
-                                                .requestMatchers("/barbero/**").hasRole("barbero")
+                        // Rutas para barbero
+                        .requestMatchers("/barbero/**").hasRole("barbero")
 
-                                                // Cualquier otra ruta debe ser autenticada
-                                                .anyRequest().authenticated())
+                        // Cualquier otra ruta debe ser autenticada
+                        .anyRequest().authenticated())
 
-                                .formLogin(form -> form
-                                                .loginPage("/login") // Página personalizada de login
-                                                .permitAll() // Permitir acceso sin autenticación a la página de login
-                                                .defaultSuccessUrl("/index", true) // Redirigir a /index después de
-                                                                                   // login exitoso
-                                )
+                .formLogin(form -> form
+                        .loginPage("/login") // Página personalizada de login
+                        .permitAll() // Permitir acceso sin autenticación a la página de login
+                        .successHandler(customAuthenticationSuccessHandler())) // Handler personalizado
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // URL de logout
+                        .logoutSuccessUrl("/index?logout") // Redirigir a /index con el parámetro logout
+                        .invalidateHttpSession(true) // Invalida la sesión
+                        .deleteCookies("JSESSIONID") // Elimina la cookie JSESSIONID
+                        .permitAll()) // Permitir acceso sin autenticación al logout
 
-                                // Configuración del logout
-                                .logout(logout -> logout
-                                                .logoutUrl("/logout") // URL de logout
-                                                .logoutSuccessUrl("/index?logout") // Redirigir a /index con el
-                                                                                   // parámetro logout
-                                                .invalidateHttpSession(true) // Invalida la sesión
-                                                .deleteCookies("JSESSIONID") // Elimina la cookie JSESSIONID
-                                                .permitAll() // Permitir acceso sin autenticación al logout
-                                )
+                .build();
+    }
 
-                                .exceptionHandling(exception -> exception
-                                                .accessDeniedPage("/Permisos")) // Página de acceso denegado
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-                                .build();
-        }
-
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-                return new BCryptPasswordEncoder();
-        }
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        return new CustomAuthenticationSuccessHandler();
+    }
 }
