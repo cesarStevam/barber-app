e576f66dc86980ce106eb68881dd685f67a0a0f6
JeanD100 <detessieres2002@gmail.com>
2024-11-22 01:06:57 -0500

    Puse alerta a Numero de documento que ya existe y puse en el footer un mapa que ya despues se configura.

--- src/main/java/com/app/app/repository/RolRepository.java
+++ src/main/java/com/app/app/repository/RolRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.app.app.entity.Rol;
 
 @Repository("rolRepository")
-public interface RolRepository extends JpaRepository<Rol, Long>{
+public interface RolRepository extends JpaRepository<Rol, Long> {
 
     Optional<Rol> findByNombreRol(String nombreRol);
 }
