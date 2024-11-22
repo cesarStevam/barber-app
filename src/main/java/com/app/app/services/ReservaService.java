11680b2454dcbf6ba5ce606c6d924f023dce8942
JeanD100 <detessieres2002@gmail.com>
2024-11-22 17:35:33 -0500

    Poner en el footer las redes Sociales y centrar mas el texto

--- src/main/java/com/app/app/services/ReservaService.java
+++ src/main/java/com/app/app/services/ReservaService.java
@@ -9,27 +9,26 @@ import org.springframework.stereotype.Service;
 import com.app.app.entity.Reserva;
 import com.app.app.repository.ReservaRepository;
 
-
 @Service("reservaService")
 public class ReservaService {
 
     @Autowired
     ReservaRepository reservaRepository;
-    
-    public List <Reserva> getReservas(){   //List = Select(Mysql)
-    return reservaRepository.findAll();
+
+    public List<Reserva> getReservas() { // List = Select(Mysql)
+        return reservaRepository.findAll();
     }
 
-    public Optional<Reserva> getReservaById(Long idReservas){
-    return reservaRepository.findById(idReservas);
-}
+    public Optional<Reserva> getReservaById(Long idReservas) {
+        return reservaRepository.findById(idReservas);
+    }
 
     public void saveOrUpdate(Reserva reserva) {
-    reservaRepository.save(reserva);
-}
+        reservaRepository.save(reserva);
+    }
 
-    public void eliminarReserva(Long idReservas){
-    reservaRepository.deleteById(idReservas);
-}
+    public void eliminarReserva(Long idReservas) {
+        reservaRepository.deleteById(idReservas);
+    }
 
 }
